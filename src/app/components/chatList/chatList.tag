1edc1ee00185c04fe3cd8156fc63c097b3ff84cf
rubencosta <rmc@nubolab.org>
2015-08-06 09:13:26 +0100

    chat:  message store + update open chat action initial implementation

--- src/app/components/chatList/chatList.tag
+++ src/app/components/chatList/chatList.tag
@@ -1,9 +1,30 @@
+import messageStore from '../../stores/messageStore';
+
 <chat-list>
     <div>
         <span>Chats</span>
         <nav>
-            <a href="">chat link1</a>
-            <a href="">chat link2</a>
+            <a each={chats} href="#" onclick={parent.updateOpenChatID} class={active: active.call(this)} > {user.name} </a>
         </nav>
     </div>
+
+    <style scoped>
+        a.active {
+            background-color: #000066;
+        }
+    </style>
+
+    this.on('update', function(){
+        this.openChatID = messageStore.getOpenChatID();
+        this.chats = messageStore.getAllChats();
+    })
+
+    this.active = function(){
+        return this.openChatID === this.id;
+    };
+
+    this.updateOpenChatID = function(e) {
+        messageStore.trigger('updateOpenChatID', e.item.id);
+    }
+
 </chat-list>
---
+++ src/app/stores/messageStore.js
@@ -0,0 +1,117 @@
+'use strict';
+
+import chatActions from '../actions/chatActions';
+
+var chats = [
+    {
+        id: 1,
+        user: {
+            profilePicture: 'https://avatars0.githubusercontent.com/u/7922109?v=3&s=460',
+            id: 2,
+            name: 'Ryan Clark',
+            status: 'online'
+        },
+        lastAccess: {
+            recipient: 1424469794050,
+            currentUser: 1424469794080
+        },
+        messages: [
+            {
+                contents: 'Hey!',
+                from: 2,
+                timestamp: 1424469793023
+            },
+            {
+                contents: 'Hey, what\'s up?',
+                from: 1,
+                timestamp: 1424469794000
+            }
+        ]
+    },
+    {
+        id: 2,
+        user: {
+            read: true,
+            profilePicture: 'https://avatars3.githubusercontent.com/u/2955483?v=3&s=460',
+            name: 'Jilles Soeters',
+            id: 3,
+            status: 'online'
+        },
+        lastAccess: {
+            recipient: 1424352522000,
+            currentUser: 1424352522080
+        },
+        messages: [
+            {
+                contents: 'Want a game of ping pong?',
+                from: 3,
+                timestamp: 1424352522000
+            }
+        ]
+    },
+    {
+        id: 3,
+        user: {
+            name: 'Todd Motto',
+            id: 4,
+            profilePicture: 'https://avatars1.githubusercontent.com/u/1655968?v=3&s=460',
+            status: 'online'
+        },
+        lastAccess: {
+            recipient: 1424423579000,
+            currentUser: 1424423574000
+        },
+        messages: [
+            {
+                contents: 'Please follow me on twitter I\'ll pay you',
+                timestamp: 1424423579000,
+                from: 4
+            }
+        ]
+    }
+];
+
+var openChatID = parseInt(chats[0].id, 10);
+
+class MessageStore extends riot.observable{
+    constructor() {
+        super(this);
+        for (var key in chatActions) {
+            let action = chatActions[key];
+            this.on(action, this.actionHandler[action])
+        }
+    }
+
+
+    getOpenChatID() {
+        return openChatID;
+    }
+
+    getChatById(id) {
+        chats.forEach((chat) => {
+            return chat.id === id && chat;
+        })
+    }
+
+    getAllChats() {
+        return chats;
+    }
+
+}
+
+MessageStore.prototype.actionHandler = {
+    updateOpenChatID: function (id) {
+        openChatID = id;
+        chats.forEach((chat) => {
+            if (chat.id === id) {
+                chat.lastAccess.currentUser = +new Date();
+            }
+        });
+
+        this.trigger('updated');
+    }
+};
+
+var messageStore = new MessageStore();
+
+module.exports = messageStore;
